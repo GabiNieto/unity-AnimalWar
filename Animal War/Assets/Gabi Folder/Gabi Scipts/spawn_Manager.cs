d1dc8e84f7369ac5cdf92d060f3c474c46153558
GabiNieto <gabi.nieto00@gmail.com>
2026-07-19 08:59:26 -0500

    10 waves added

--- Animal War/Assets/Gabi Folder/Gabi Scipts/spawn_Manager.cs
+++ Animal War/Assets/Gabi Folder/Gabi Scipts/spawn_Manager.cs
@@ -6,7 +6,9 @@ public class spawn_Manager : MonoBehaviour
     public GameObject animalBunny;
     public GameObject animalDeer;
     public GameObject animalTiger;
-    public GameObject animalSUPERMONKEY;
+    public GameObject animalMonkey;
+    public GameObject animalElephant;
+    public GameObject animalCatipiller;
     public Transform spawnPoint;
 
     public float waveInterval = 10f;
@@ -14,6 +16,9 @@ public class spawn_Manager : MonoBehaviour
     private float tigerSpawnTime = 0f;
     private float bunnySpawnTime = 0f;
     private float deerSpawnTime = 0f;
+    private float monkeySpawnTime = 0f;
+    private float elephantSpawnTime = 0f;
+    private float catipillerSpawnTime = 0f;
 
     void Start() //Start it up
     {
@@ -32,13 +37,23 @@ public class spawn_Manager : MonoBehaviour
         yield return new WaitForSeconds(waveInterval);
         yield return StartCoroutine(Wave5());
         yield return new WaitForSeconds(waveInterval);
+        yield return StartCoroutine(Wave6());
+        yield return new WaitForSeconds(waveInterval);
+        yield return StartCoroutine(Wave7());
+        yield return new WaitForSeconds(waveInterval);
+        yield return StartCoroutine(Wave8());
+        yield return new WaitForSeconds(waveInterval);
+        yield return StartCoroutine(Wave9());
+        yield return new WaitForSeconds(waveInterval);
+        yield return StartCoroutine(Wave10());
+        yield return new WaitForSeconds(waveInterval);
     }
 
     IEnumerator Wave1()
     {
-        for ( int i = 0; i < 22; i++)
+        for ( int i = 0; i < 5; i++)
         {
-            bunnySpawnTime = 2;
+            bunnySpawnTime = 1;
             yield return StartCoroutine(SpawnBunny());
         }
     }
@@ -47,10 +62,10 @@ public class spawn_Manager : MonoBehaviour
     {
         for (int i = 0; i < 3; i++)
         {
-            bunnySpawnTime = 2;
+            bunnySpawnTime = 1;
             yield return StartCoroutine(SpawnBunny());
         }
-        deerSpawnTime = 2.5f;
+        deerSpawnTime = 1;
         yield return StartCoroutine(SpawnDeer());
     }
 
@@ -59,8 +74,8 @@ public class spawn_Manager : MonoBehaviour
     {
         for (int i = 0; i < 3; i++)
         {
-            bunnySpawnTime = 2;
-            deerSpawnTime = 2f;
+            bunnySpawnTime = 1;
+            deerSpawnTime = 1;
             yield return StartCoroutine(SpawnBunny());
             yield return StartCoroutine(SpawnDeer());
         }
@@ -71,7 +86,7 @@ public class spawn_Manager : MonoBehaviour
     {
         for (int i = 0; i < 3; i++)
         {
-            bunnySpawnTime = 2;
+            bunnySpawnTime = 1;
             yield return StartCoroutine(SpawnBunny());
         }
         tigerSpawnTime = 1f;
@@ -81,7 +96,7 @@ public class spawn_Manager : MonoBehaviour
 
     IEnumerator Wave5()
     {
-        deerSpawnTime = 2f;
+        deerSpawnTime = 1f;
         yield return StartCoroutine(SpawnDeer());
         yield return StartCoroutine(SpawnDeer());
         for (int i = 0; i < 2; i++)
@@ -89,6 +104,106 @@ public class spawn_Manager : MonoBehaviour
             tigerSpawnTime = 2;
             yield return StartCoroutine(SpawnTiger());
         }
+    }
+
+    IEnumerator Wave6()
+    {
+        monkeySpawnTime = 1f;
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        elephantSpawnTime = 1;
+        yield return StartCoroutine(SpawnElephant());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        tigerSpawnTime = 2;
+        yield return StartCoroutine(SpawnTiger());
+    }
+
+    IEnumerator Wave7()
+    {
+        tigerSpawnTime = 2;
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        elephantSpawnTime = 1;
+        yield return StartCoroutine(SpawnElephant());
+        yield return StartCoroutine(SpawnElephant());
+        yield return StartCoroutine(SpawnTiger());
+    }
+
+    IEnumerator Wave8()
+    {
+        catipillerSpawnTime = 1f;
+        yield return StartCoroutine(SpawnCatipiller());
+        yield return StartCoroutine(SpawnCatipiller());
+        monkeySpawnTime = 1f;
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        tigerSpawnTime = 2;
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        catipillerSpawnTime = 1f;
+        yield return StartCoroutine(SpawnCatipiller());
+        yield return StartCoroutine(SpawnCatipiller());
+        monkeySpawnTime = 1f;
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        tigerSpawnTime = 2;
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+
+
+    }
+
+    IEnumerator Wave9()
+    {
+        catipillerSpawnTime = 1f;
+        yield return StartCoroutine(SpawnCatipiller());
+        yield return StartCoroutine(SpawnCatipiller());
+        tigerSpawnTime = 0.5f;
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        catipillerSpawnTime = 1f;
+        yield return StartCoroutine(SpawnCatipiller());
+        yield return StartCoroutine(SpawnCatipiller());
+    }
+
+    IEnumerator Wave10()
+    {
+        elephantSpawnTime = 0.5f;
+        yield return StartCoroutine(SpawnElephant());
+        yield return StartCoroutine(SpawnElephant());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnElephant());
+        tigerSpawnTime = 0.5f;
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnTiger());
+        yield return StartCoroutine(SpawnElephant());
+        yield return StartCoroutine(SpawnTiger());
+        monkeySpawnTime = 0.25f;
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+        yield return StartCoroutine(SpawnMonkey());
+
     }
 
 
@@ -108,5 +223,23 @@ public class spawn_Manager : MonoBehaviour
     {
         yield return new WaitForSeconds(deerSpawnTime);
         Instantiate(animalDeer, spawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator SpawnMonkey()
+    {
+        yield return new WaitForSeconds(monkeySpawnTime);
+        Instantiate(animalMonkey, spawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator SpawnElephant()
+    {
+        yield return new WaitForSeconds(elephantSpawnTime);
+        Instantiate(animalElephant, spawnPoint.position, Quaternion.identity);
+    }
+
+    IEnumerator SpawnCatipiller()
+    {
+        yield return new WaitForSeconds(catipillerSpawnTime);
+        Instantiate(animalCatipiller, spawnPoint.position, Quaternion.identity);
     }
 }
